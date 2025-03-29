aedbdcb39ea5b38d1525507f8bd396e212bf3545
Turann_ <turanull000@gmail.com>
2025-03-29 22:45:43 +0300

    Initial Commit

---
+++ demoapp/demoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoappApp.swift
+//  demoapp
+//
+//  Created by Turann_ on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct demoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
