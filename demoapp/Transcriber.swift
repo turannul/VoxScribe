ae8d93331fc030302a343d10c7a95a15a7ac74f6
Turann_ <turanull000@gmail.com>
2025-03-30 04:46:22 +0300

    uh

--- demoapp/Transcriber.swift
+++ demoapp/Transcriber.swift
@@ -7,13 +7,11 @@
 
 
 import SwiftUI
-import Speech
+import WhisperKit // we need to make use of this. because Apple's Speech recognition randomly stops working.
 
 class Transcriber: NSObject {
-    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
     private var previousTranscription = ""
-    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
-    private var recognitionTask: SFSpeechRecognitionTask?
+
     
     private var _transcribedText = ""
     var transcribedText: String {
@@ -33,11 +31,6 @@ class Transcriber: NSObject {
     }
     
     func setupRecognition() {
-        SFSpeechRecognizer.requestAuthorization { status in
-            if status != .authorized {
-                print("Speech recognition authorization denied")
-            }
-        }
     }
     
     func processAudio(buffer: AVAudioPCMBuffer) {
@@ -49,35 +42,15 @@ class Transcriber: NSObject {
     }
     
     func processWithSpeechRecognition(buffer: AVAudioPCMBuffer) {
-        if recognitionRequest == nil {
-            recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
-            
-            guard let recognitionRequest = recognitionRequest else { return }
-            
-            recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest) { result, error in
-                if let result = result {
-                    let newText = result.bestTranscription.formattedString
-                    let addedText = String(newText.suffix(max(0, newText.count - self.previousTranscription.count)))
-                    DispatchQueue.main.async {
-                        self.transcribedText += addedText
-                        self.previousTranscription = newText
-                    }
-                }
-            }
-        }
-        recognitionRequest?.append(buffer)
+
     }
     
     func finishProcessing() {
-        recognitionRequest?.endAudio()
-        recognitionRequest = nil
-        recognitionTask?.cancel()
-        recognitionTask = nil
     }
 }
     
     func transcribe(_ buffer: AVAudioPCMBuffer) async throws -> TranscriptionResult {
-        return TranscriptionResult(text: "Sample transcription text")
+        return TranscriptionResult(text: "...")
     }
 
 struct TranscriptionResult {
